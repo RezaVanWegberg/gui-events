fb4ce3b40595f257faedca660005d977b24bd4e6
Reza <99068159@mydavinci.nl>
2022-05-05 22:06:15 +0200

    heb clicker v3 gemaakt en een clear function toegevoegd

---
+++ ClickerV3.py
@@ -0,0 +1,101 @@
+from distutils.command.config import config
+import tkinter as tk
+from turtle import window_width
+
+Score = 0
+
+def Plus():
+    global Score
+    Score += 1
+    print(Score)
+    label['text'] = Score
+    if Score > 0:
+        window.config(bg='green')
+    if Score == 0:
+        window.config(bg='grey')
+    if Score < 0:
+        window.config(bg='red')
+    
+
+def Min():
+    global Score
+    Score -= 1
+    print(Score)
+    label['text'] = Score
+    if Score > 0:
+        window.config(bg='green')
+    if Score == 0:
+        window.config(bg='grey')
+    if Score < 0:
+        window.config(bg='red')
+
+window = tk.Tk()
+window.title("test")
+window.geometry('500x200')
+window.config(bg='pink')
+
+LaatstUp = 0
+def ChangeLaatstUp(event):
+    global LaatstUp
+    global LaatstDown
+    LaatstUp = 1
+    LaatstDown = 0
+    print(f"up {LaatstUp}")
+    print(f"down {LaatstDown}")
+
+LaatstDown = 0
+def ChangeLaatstDown(event):
+    global LaatstUp
+    global LaatstDown
+    LaatstUp = 0
+    LaatstDown = 1
+    print(f"up {LaatstUp}")
+    print(f"down {LaatstDown}")
+    
+buttonUp = tk.Button(window, text="Up", command=Plus)
+buttonUp.pack(ipadx=10,ipady=10, fill='x', expand=True)
+buttonUp.bind("<Button>", ChangeLaatstUp)
+
+
+label = tk.Label(window, text= 0)
+label.pack(ipadx=10,ipady=10, fill='x', expand=True)
+
+def MultiplyAndDivide(event):
+    global Score
+    if LaatstUp == 1:
+        Score *= 3
+        label['text'] = Score
+    elif LaatstDown == 1:
+        Score /= 3
+        label['text'] = Score
+
+label.bind("<Double-Button>", MultiplyAndDivide)
+
+def ClearScore(event):
+    global Score
+    Score = 0
+    label['text'] = Score
+
+label.bind("<Triple-Button-3>", ClearScore)
+
+buttonDown = tk.Button(window, text="Down", command=Min)
+buttonDown.pack(ipadx=10,ipady=10, fill='x', expand=True)
+buttonDown.bind("<Button>", ChangeLaatstDown)
+
+
+def ChangeEnter(event):
+    window.config(bg="yellow")
+
+label.bind("<Enter>", ChangeEnter)
+
+def ChangeLeave(event):
+    if Score > 0:
+        window.config(bg='green')
+    if Score == 0:
+        window.config(bg='grey')
+    if Score < 0:
+        window.config(bg='red')
+
+label.bind("<Leave>", ChangeLeave)    
+
+window.mainloop()
